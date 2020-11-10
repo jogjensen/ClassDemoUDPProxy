d97f41e77d042674706ca8571466a1a9d4bb691a
Peter Levinsky <pele@easj.dk>
2020-11-10 08:48:36 +0100

    Add project files.

---
+++ ClassDemoUDPProxy/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ClassDemoUDPProxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProxyWorker worker = new ProxyWorker();
+            worker.Start();
+
+            Console.ReadLine();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ ClassDemoUDPProxy/ProxyWorker.cs
@@ -0,0 +1,39 @@
+﻿namespace ClassDemoUDPProxy
+{
+    class ProxyWorker
+    {
+        public ProxyWorker()
+        {
+        }
+
+        public async void Start()
+        {
+            while (true)
+            {
+                SomeClass obj = ReadUDPPacket();
+                SendToRest(obj);
+            }
+
+        }
+
+
+        public SomeClass ReadUDPPacket()
+        {
+            // udp - Receive
+
+            return null;
+        }
+
+        public async void SendToRest(SomeClass obj)
+        {
+            // http client - PostAsync
+
+        }
+    }
+
+    // dummy
+    class SomeClass
+    {
+
+    }
+}
